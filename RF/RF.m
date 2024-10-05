48b1a3a38b3ce836438577ebff13cd5bd64303e4
DongmeiXuGeo <125113318+DongmeiXuGeo@users.noreply.github.com>
2024-10-05 16:26:38 +0800

    Update RF.m

--- RF/RF.m
+++ RF/RF.m
@@ -2,20 +2,19 @@ clear;
 clc;
 
 data = xlsread('F:\0.TQP\data2\habitat\ANN\data.xlsx');
-data = data(randperm(size(data,1)),:); %随机划分
-ind = round(0.8 * size(data,1)); %按比例分
+data = data(randperm(size(data,1)),:); %Random partition
+ind = round(0.8 * size(data,1)); %Proportional division
 %data= data(randperm(length(data)));
-trainData = data(1:ind, 1:end); %训练集
-testData = data(ind+1:end, 1:end); %测试集
+trainData = data(1:ind, 1:end); %Training set
+testData = data(ind+1:end, 1:end); %Test set
 
 Y = trainData(:,12);
 X = trainData(:,2:11);
 Y_test = testData(:,12);
 X_test = testData(:,2:11);
 isCategorical = [zeros(15,1);ones(size(X,2)-15,1)]; 
-%% 最优leaf选择
-% 对于回归，一般规则是将叶子大小设置为5。通过比较不同叶子数量MSE获得最佳叶子数量
-% 叶子数量越少MSE越小，即使如此也肯定不是越小越好，这里就假设leaf=5是最优了
+%% Optimal leaf selection
+% For regression, the general rule is to set the leaf size to 5. The optimal leaf quantity was obtained by comparing different leaf quantity MSE
 leaf = [5 10 20 50 100];
 col = 'rbcmy';
 figure
@@ -30,8 +29,8 @@ xlabel('Number of Grown Trees')
 ylabel('Mean Squared Error') 
 legend({'5' '10' '20' '50' '100'},'Location','NorthEast')
 hold off
-% 本研究leaf=20是最优
-%% 树的数量设置，前面用了50棵树（为了收敛速度快），接下来增加到100
+% In this study, leaf=20 is optimal
+%% The number of trees is set to 50 (for faster convergence), and then increased to 100...
 ntrees = [50 100 200 300 400 500];
 col = 'rgbmyk';
 figure
@@ -46,9 +45,9 @@ xlabel('Number of Grown Trees')
 ylabel('Mean Squared Error') 
 legend({'50' '100' '200' '300' '400' '500'},'Location','NorthEast')
 hold off
-% 本研究ntrees=500是最优
+% In this study, ntrees=500 is the best
 
-%% 训练随机森林，TreeBagger使用内容，以及设置随机森林参数
+%% Train random forest and set random forest parameters
 tic
 leaf = 20;
 ntrees = 500;
@@ -57,14 +56,14 @@ disp('Training the tree bagger')
 b = TreeBagger(ntrees, X,Y, 'Method','regression', 'oobvarimp','on', 'surrogate', 'on', 'minleaf',leaf,'FBoot',fboot);
 toc
 
-%% 使用训练好的模型进行预测
+%% Use the trained model to make predictions
 disp('Estimate Output using tree bagger')
 x = Y_test;
 y = predict(b, X_test);
 toc
 
 % calculate the training data correlation coefficient
-% 计算相关系数
+% Calculating correlation coefficient
 cct=corrcoef(x,y);
 cct=cct(2,1);
 
@@ -88,8 +87,8 @@ fn='ScatterDiagram';
 fnpng=[fn,'.png'];
 print('-dpng',fnpng);
 
-%% 预测全域
-%输入tif数据
+%% Predict the whole study area
+%Importing tif data
 [A1,R]=geotiffread('F:\0.TQP\data2\yinzitif\585\gyhbio10.tif');
 [A2,R]=geotiffread('F:\0.TQP\data2\yinzitif\585\gyhbio13.tif');
 [A3,R]=geotiffread('F:\0.TQP\data2\yinzitif\585\gyhbio14.tif');
@@ -116,20 +115,20 @@ AA(AA<-1000000)=nan;
 AA(AA==15)=nan;
 pred=AA;
 
-%随机森林
+%Random forest
 disp('Estimate Output using tree bagger')
 y_pred = predict(b, pred);
 toc
-%输出数据
+%Output data
 r=y_pred';
 re=(r-min(r))/(max(r)-min(r));
 re=reshape(re',1974,2611);
 info = geotiffinfo('F:\0.TQP\Data\bio\126\gyhtif\bio3.tif');
-filename = strcat('F:\0.TQP\data2\habitat\RF\syx585.tif'); % 栅格数据的每个文件地址+文件名
+filename = strcat('F:\0.TQP\data2\habitat\RF\syx585.tif');
 geotiffwrite(filename,re,R,'GeoKeyDirectoryTag',info.GeoTIFFTags.GeoKeyDirectoryTag);
-%% 自变量重要性分析
-% 自变量对RF模型贡献有大有小，RF的预测能力依赖于贡献大的自变量。对于每个自变量，可以观察其重要性，进行取舍组合，并查看MSE是否有改善。
-% OOBPermutedPredictorDeltaError提供了每个自变量的重要性，值越大，变量越重要。
+
+%% Importance analysis of independent variables
+% OOBPermutedPredictorDeltaError provides the importance of each independent variable, the greater the value, the more important the variable.
 figure
 bar(b.OOBPermutedPredictorDeltaError)
 xlabel('Feature Number') 
